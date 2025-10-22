bb5a8e2355f39ac2bcfd256e1df78294a9330004
katzi.lipsiz <katzilipsiz@gmail.com>
2025-10-22 11:17:59 +0500

    Develop (#4)
    * #1. Структура проекта
    
    * #2 Добавление логина
    
    * #3. Макет LoginPage
    
    * #4. Начало работы с главным окном
    
    * #5. Баг с изображением исправлен
    
    * #6. Мелкие изменения
    
    * #7. Добавление темной темы
    
    * #5. Баг с изображением исправлен
    
    * #8. Структура проекта

---
+++ Katastata/Windows/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Katastata.UserControls;
+
+namespace Katastata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ApplyTheme(string themeName)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            var themeUri = new Uri($"Assets/Themes/{themeName}.xaml", UriKind.Relative);
+            var themeDict = new ResourceDictionary { Source = themeUri };
+
+            Application.Current.Resources.MergedDictionaries.Add(themeDict);
+        }
+
+        private void CloseButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+    }
+}
