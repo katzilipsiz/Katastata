728da6cf289a4bb9ba9ef0983bc59a6f166c30a3
Катзи <katzilipsiz@gmail.com>
2025-11-11 19:07:26 +0500

    #21. Работает всё!

--- Katastata/AuthWindow.xaml.cs
+++ Katastata/AuthWindow.xaml.cs
@@ -10,50 +10,36 @@ namespace Katastata
     {
         private readonly AppDbContext _db;
         private readonly UserViewModel _vm;
-
         public int LoggedInUserId { get; private set; } = -1;
 
         public AuthWindow(DbContextOptions<AppDbContext> options)
         {
             InitializeComponent();
-
             _db = new AppDbContext(options);
             _vm = new UserViewModel(_db);
-
-            // Подписываемся на событие LoginSuccessful — VM вызовет его при успехе
             _vm.LoginSuccessful += OnLoginSuccessful;
-
-            // Показываем страницу логина по умолчанию:
             ShowLoginPage(null, null);
-
             DataContext = _vm;
         }
 
         private void OnLoginSuccessful(int userId)
         {
-            // Отладочное сообщение — если понадобится
             Dispatcher.Invoke(() =>
             {
                 LoggedInUserId = userId;
-                // Устанавливаем DialogResult = true только когда окно открыто как диалог
-                DialogResult = true; // <-- должно сработать только из ShowDialog
+                DialogResult = true;
                 Close();
             });
         }
 
-        // Методы переключения контента — убедись, что в XAML есть ContentControl с x:Name="ContentArea"
         private void ShowLoginPage(object sender, RoutedEventArgs e)
         {
-            var login = new LoginPage();
-            login.DataContext = _vm;
-            ContentArea.Content = login;
+            ContentArea.Content = new LoginPage { DataContext = _vm };
         }
 
         private void ShowRegisterPage(object sender, RoutedEventArgs e)
         {
-            var reg = new RegisterPage();
-            reg.DataContext = _vm;
-            ContentArea.Content = reg;
+            ContentArea.Content = new RegisterPage { DataContext = _vm };
         }
     }
 }
