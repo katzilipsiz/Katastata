d666bd8a9285ea8a34cf8ed843ccc68ed1da32dd
Катзи <katzilipsiz@gmail.com>
2025-11-13 15:00:59 +0500

    #35. Фильтрация сессий и статистики

--- Katastata/ViewModels/StatisticsViewModel.cs
+++ Katastata/ViewModels/StatisticsViewModel.cs
@@ -1,32 +1,48 @@
 ﻿using Katastata.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
 
 namespace Katastata.ViewModels
 {
-    public class StatisticsViewModel
+    public class StatisticsViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<StatisticsViewModelItem> Statistics { get; } = new ObservableCollection<StatisticsViewModelItem>();
+        public ICollectionView FilteredStatistics { get; }
+
+        private string _filterText;
+        public string FilterText
+        {
+            get => _filterText;
+            set
+            {
+                _filterText = value;
+                OnPropertyChanged();
+                FilteredStatistics.Refresh();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
 
         public StatisticsViewModel(List<Statistics> stats)
         {
             foreach (var stat in stats)
-            {
                 Statistics.Add(new StatisticsViewModelItem(stat));
-            }
+
+            FilteredStatistics = CollectionViewSource.GetDefaultView(Statistics);
+            FilteredStatistics.Filter = FilterStatistics;
         }
-    }
 
-    public class StatisticsViewModelItem
-    {
-        public string ProgramName { get; set; }
-        public TimeSpan TotalTime { get; set; }
-        public DateTime? LastLaunch { get; set; }
-
-        public StatisticsViewModelItem(Statistics stat)
+        private bool FilterStatistics(object item)
         {
-            ProgramName = stat.Program?.Name ?? "Unknown";
-            TotalTime = stat.TotalTime;
-            LastLaunch = stat.LastLaunch;
+            if (string.IsNullOrWhiteSpace(FilterText)) return true;
+            var statItem = item as StatisticsViewModelItem;
+            return statItem.ProgramName.Contains(FilterText, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
