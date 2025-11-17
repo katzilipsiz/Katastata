f6cbfe94ba5d61401dbd2399fd849791300225cf
katzilipsiz <katzilipsiz@gmail.com>
2025-11-17 15:59:52 +0500

    #37. Изменение имен программ

--- Katastata/ProgramDetailsWindow.xaml.cs
+++ Katastata/ProgramDetailsWindow.xaml.cs
@@ -2,6 +2,7 @@
 using Katastata.Services;
 using Katastata.ViewModels;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Katastata
 {
@@ -9,10 +10,21 @@ namespace Katastata
     {
         public ProgramDetailsWindow(Program program, int userId, AppMonitorService service)
         {
-            InitializeComponent();
+            InitializeComponent(); // Сначала инициализация XAML
+
             var sessions = service.GetSessions(userId).Where(s => s.ProgramId == program.Id).ToList();
             var stat = service.GetStatistics(userId).FirstOrDefault(st => st.ProgramId == program.Id);
-            DataContext = new ProgramDetailsViewModel(program, sessions, stat, service);
+
+            DataContext = new ProgramDetailsViewModel(program, sessions, stat, service); // Потом DataContext
+        }
+
+        private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.Focus();
+                e.Handled = true;
+            }
         }
     }
 }
